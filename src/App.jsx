243e303d862c902d8be6d45a17e384b1cb64ef29
Ljiljana J <ljiljajanjic@gmail.com>
2024-03-04 20:31:45 +0100

    Lesson 1.2:Homework update:creating two separate files, TodoList and TodoForm

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import './TodoList'
-import AddTodoForm from './TodoList'
-
-
-const todoList = [
-  {
-    id: 1,
-    title: 'wake up at 9am',
-  },
-  {
-    id: 2,
-    title: 'bathroom routine',
-  },
-  {id: 3,
-    title: 'prepare and have breakfast'
-  },
-];
+import AddTodoForm from './AddTodoForm'
+import TodoList from './TodoList'
 
 function App() {
 
@@ -26,15 +11,7 @@ function App() {
     <div>
   <h1>Todo List</h1>
   < AddTodoForm />
-  <ul>
-    {todoList.map( function (list) {
-      return (
-      <li key={list.id}>
-        <span>{list.title}</span>
-        </li>
-      );
-})}
-  </ul>
+  < TodoList />
   </div>
   );
 }
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,11 +1,36 @@
-function AddTodoForm () {
+const todoList = [
+    {
+      id: 1,
+      title: 'wake up on time',
+    },
+    {
+      id: 2,
+      title: 'bathroom routine',
+    },
+    {id: 3,
+      title: 'prepare and eat breakfast'
+    },
+  ];
+
+
+function TodoList () {
     return (
-        <form>
-          <label htmlFor="title">Title</label>
-          <input type="text" id="todoTitle" />
-          <button type="submit">Add</button>
-        </form>
+        <div>
+    <ul>
+    {todoList.map( function (list) {
+      return (
+      <li key={list.id}>
+        <span>{list.title}</span>
+        </li>
       );
+})}
+  </ul>
+  </div>
+    );
 };
 
-export default AddTodoForm;
+
+
+
+
+export default TodoList;
