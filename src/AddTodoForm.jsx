1b7cc98cbdc2b5ad8ac3e6122152a6a301bf1bf8
Ljiljana J <ljiljajanjic@gmail.com>
2024-04-05 21:18:27 +0200

    Created Reusable Input with Label Component

--- src/AddTodoForm.jsx
+++ src/AddTodoForm.jsx
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import InputWithLabel from './InputWithLabel';
 
 function AddTodoForm ({ onAddTodo }) {
   const [todoTitle, setTodoTitle] = useState("");
@@ -18,8 +19,14 @@ function AddTodoForm ({ onAddTodo }) {
   };
     return (
         <form onSubmit={handleAddTodo}>
-          <label htmlFor="todoTitle">Title: </label>
-          <input type="text" id="todoTitle" name="title" value={todoTitle} onChange={handleTitleChange} />
+        <InputWithLabel
+          label="Title:"
+          type="text"
+          id="todoTitle"
+          name="title"
+          value={todoTitle}
+          onChange={handleTitleChange}
+          />
           <button type="submit">Add</button>
         </form>
       );
---
+++ src/InputWithLabel.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const InputWithLabel = (props) => {
+  return (
+    <>
+    <label htmlFor={props.id}>{props.label}</label>
+          <input 
+          type={props.type} 
+          id={props.id}
+          name={props.name}
+          value={props.value} 
+          onChange={props.onChange} 
+          />
+    </>
+  );
+};
+
+export default InputWithLabel;
