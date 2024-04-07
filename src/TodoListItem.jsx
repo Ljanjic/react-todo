00ff7100282db7fb80ce0a0db80c15f33def06d2
Ljiljana J <ljiljajanjic@gmail.com>
2024-04-07 20:26:37 +0200

    Added Remove Button to List Items

--- src/TodoListItem.jsx
+++ src/TodoListItem.jsx
@@ -1,13 +1,16 @@
 import item from "./TodoList";
 
 
-function TodoListItem ({ todo }) {
+function TodoListItem ({ todo, onRemoveTodo }) {
+  const RemoveButton = () => {
+    onRemoveTodo(todo.id);
+  };
   return  (
     <li>
       <span>{todo.title}</span>
+      <button type="button" onClick={RemoveButton}>Remove</button>
     </li>
-        
-  )
+  );
 }
 
 export default TodoListItem;
