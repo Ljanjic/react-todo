829279989d631322a88d2e1f73efd0ad4a642f67
Ljiljana J <ljiljajanjic@gmail.com>
2024-05-21 21:01:04 -0400

    CSS Styled Components SVG

--- src/TodoListItem.jsx
+++ src/TodoListItem.jsx
@@ -1,16 +1,18 @@
-import item from "./TodoList";
+import item from './TodoList';
+import styles from './TodoListItem.module.css';
 
-
-function TodoListItem ({ todo, onRemoveTodo }) {
-  const RemoveButton = () => {
-    onRemoveTodo(todo.id);
-  };
-  return  (
-    <li>
-      <span>{todo.title}</span>
-      <button type="button" onClick={RemoveButton}>Remove</button>
-    </li>
-  );
+function TodoListItem({ todo, onRemoveTodo }) {
+    const RemoveButton = () => {
+        onRemoveTodo(todo.id);
+    };
+    return (
+        <li className={styles.ListItem}>
+            <span>{todo.title}</span>
+            <button type='button' onClick={RemoveButton}>
+                Remove
+            </button>
+        </li>
+    );
 }
 
 export default TodoListItem;
